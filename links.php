d911e1993a670dc347afc60d9212ff343d2411bb
rochelle <shielmtongol@yahoo.com>
2014-01-21 18:59:22 +0800

    Create links.php

---
+++ links.php
@@ -0,0 +1,36 @@
+<link type='text/css' rel='stylesheet' href='css/bootstrap.min.css' />
+<link type='text/css' rel='stylesheet' href='css/bootstrap.css' />
+<link type='text/css' rel='stylesheet' href='css/bootstrap-responsive.css' />
+<link type='text/css' rel='stylesheet' href='css/bootstrap-responsive.min.css' />
+<link type='text/css' rel='stylesheet' href='css/bootstrap-tables.css' />
+<link type='text/css' rel='stylesheet' href='css/bootstrap-theme.css' />
+<link type='text/css' rel='stylesheet' href='css/bootstrap-theme.min.css' />
+<link type='text/css' rel='stylesheet' href='css/docs.css' />
+<link type='text/css' rel='stylesheet' href='css/font-awesome.css' />
+<link type='text/css' rel='stylesheet' href='css/font-awesome.min.css' />
+<link type='text/css' rel='stylesheet' href='css/font-awesome-ie7.min.css' />
+<link type='text/css' rel='stylesheet' href='css/prettify.css' />
+<link type='text/css' rel='stylesheet' href='css/tablecloth.css' />
+<link type='text/css' rel='stylesheet' href='css/sliding.css' />
+<link rel="stylesheet" type="text/css" href="css/myStyle.css" />
+
+<script type='text/javascript' src='js/bootstrap.js'/></script>
+<script type="text/javascript" src='js/jquery.metadata.js'></script>
+<script type='text/javascript' src='js/bootstrap-arrows.min.js'/></script>
+<script type='text/javascript' src='js/bootstrap-arrows.js'/></script>
+<script type='text/javascript' src='js/demo.js'/></script>
+<script type='text/javascript' src='js/docs.js'/></script>
+<script type='text/javascript' src='js/jquery.js'/></script>
+<script type='text/javascript' src='js/jquery.min.js'/></script>
+<script type='text/javascript' src='js/jquery.tablecloth.js'/></script>
+<script type='text/javascript' src='js/jquery.tablesorter.js'/></script>
+<script type='text/javascript' src='js/jquery.tablesorter.min.js'/></script>
+<script type='text/javascript' src='js/jquery-1.7.2.min.js'/></script>
+<script type='text/javascript' src='js/jquery-1.8.3.min.js'/></script>
+<script type='text/javascript' src='js/jquery-1.9.0.min.js'/></script>
+<script type='text/javascript' src='js/jquery-ui-1.10.0.custom.min.js'/></script>
+<script type='text/javascript' src='js/prettify.js'/></script>
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src='js/search.js'></script>
+
+
